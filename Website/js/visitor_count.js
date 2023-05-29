681858452c458e14380c7638965d691ae22b8234
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-29 21:57:27 +0530

    wrong change corrected

--- Website/js/visitor_count.js
+++ Website/js/visitor_count.js
@@ -1,5 +1,5 @@
 
 
 fetch('https://vis-count-azure.azurewebsites.net/api/azure0visi0count?code=BKlx6E0L1nm7AIFMm80D_ywWsnAeiNx0oU0CyXC3ambaAzFuX3nFng==').then(reponse=>reponse.json()).then((data)=>{
-    document.getElementById('Visitorcount').innerText=data.count
+    document.getElementById('visitor-count').innerText=data.Visitorcount
 })
